a7fbde4357aac6fc8111736b54199b782dea8b82
EMERSON LUIZ CRUZ JUNIOR <emersonjr02@gmail.com>
2025-10-12 11:44:30 -0300

    feat: adding the structure that will be used in one pass algo

--- onePassAlgo.hpp
+++ onePassAlgo.hpp
@@ -2,8 +2,33 @@
 #include<iostream>
 #include<vector>
 #include<string>
-#include<fstream>
+#include<map>
+#include "FileHandler.hpp"
+#include "ErrorHandler.hpp"
+#include "Symbols.hpp"
 
 using namespace std;
 
-vector<vector<string>> onePass();
+class OnePassAlgo {
+
+    public:
+        OnePassAlgo(vector<vector<string>> inAsm) : asmCode(inAsm){};
+
+        void o1(){
+            //rotulo: instr args 
+
+            for(auto line : asmCode){
+
+                
+            }
+        }
+
+        FileHandler doO2(const FileHandler & fhO1){
+
+        }
+    private:
+        vector<vector<string>> asmCode;
+        map<string, tuple<int, bool, vector<int>>> symbolList;
+        vector<pair<int, short>> memo;
+        int memoPos = 0;
+};
